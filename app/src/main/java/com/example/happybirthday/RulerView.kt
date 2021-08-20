73c6bb191d5bf4dd51fff937f73b101294f53012
Pannoniae <prisznyak.zsombor@gmail.com>
2021-08-20 17:42:06 +0100

    - impl ruler

--- app/src/main/java/com/example/happybirthday/RulerView.kt
+++ app/src/main/java/com/example/happybirthday/RulerView.kt
@@ -10,23 +10,79 @@ import android.view.View
 class RulerView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
 
-        val x1 = 0.toFloat()
-        val x2 = width.toFloat()
+        val xMargin = 0.25F * cmToPixels()
+        val yMargin = 0.25F * cmToPixels()
+
+        val x1 = 0.toFloat() + xMargin
+        val x2 = width.toFloat() - yMargin
         val xMid = width / 2.toFloat()
-        val y1 = 0.toFloat()
-        val y2 = height.toFloat()
+        val y1 = 0.toFloat() + yMargin
+        val y2 = height.toFloat()  - yMargin
         val yMid = height / 2.toFloat()
 
-        val xMargin = 0.25 * cmToPixels()
-        val yMargin = 0.25 * cmToPixels()
+        val lineThickness = 0.5F * cmToPixels()
+        val thickness1 = 0.1F * cmToPixels()
+        val thickness2 = 0.05F * cmToPixels()
+        val thickness3 = 0.01F * cmToPixels()
+
+        val fontSize = 0.5F * cmToPixels()
+
+        val smallLineHeight = 1F * cmToPixels()
+        val midLineHeight = 1.25F * cmToPixels()
+        val largeLineHeight = 1.5F * cmToPixels()
 
         val line = Paint().apply {
             color = Color.BLACK
-            strokeWidth = 5F
+            strokeWidth = lineThickness
+            strokeCap = Paint.Cap.ROUND
         }
+
+        val largeLine = Paint().apply {
+            color = Color.BLACK
+            strokeWidth = thickness1
+        }
+        val midLine = Paint().apply {
+            color = Color.BLACK
+            strokeWidth = thickness2
+        }
+        val smallLine = Paint().apply {
+            color = Color.BLACK
+            strokeWidth = thickness3
+        }
+
+        val numbers = Paint().apply {
+            textSize = (0.5 * cmToPixels()).toFloat()
+        }
+        // bottom line
+        canvas.drawLine(x1, y2, x2, y2, line)
+        // numbers
         for (i in 0..getMaxDeviceSizeInCm()) {
-            canvas.drawLine(x1, y1, x2, y1, line)
+            canvas.drawText(
+                i.toString(), (i * cmToPixels()).toFloat() + (x1 / 2),
+                y2 - largeLineHeight - yMargin, numbers
+            )
+        }
+        // the lines
+        for (i in (0..getMaxDeviceSizeInCm() * 10)) {
+            // i is in mm, j is in cm
+            val j = i / 10;
+            var theLineHeight: Float;
+            var line: Paint;
+            if (i % 10 == 0) {
+                theLineHeight = largeLineHeight
+                line = largeLine
+            } else if (i % 5 == 0) {
+                theLineHeight = midLineHeight
+                line = midLine
+            } else {
+                theLineHeight = smallLineHeight
+                line = smallLine
+            }
+
+            canvas.drawLine(
+                (i * mmToPixels()).toFloat() + x1, y2,
+                (i * mmToPixels()).toFloat() + x1, (y2 - theLineHeight), line
+            )
         }
     }
-
 }
--- app/src/main/java/com/example/happybirthday/ScreenUtils.kt
+++ app/src/main/java/com/example/happybirthday/ScreenUtils.kt
@@ -3,6 +3,9 @@ package com.example.happybirthday
 fun cmToPixels(): Int {
     return 50
 }
+fun mmToPixels(): Int {
+    return cmToPixels() / 10
+}
 
 fun getMaxDeviceSizeInCm(): Int {
     return 50 * 15;
